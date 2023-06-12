c10e333a74c96871a3fd77fd24ef5641732e56ed
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-12 22:44:10 +0530

    12-06

---
+++ Loops.py
@@ -0,0 +1,52 @@
+# while loop->
+
+# age = int(input("Enter your Age:"))
+
+# while age < 0:
+#     print("Age can't be negative")
+#     age = int(input("Enter your Age:"))
+# print(f"You are {age} years old")
+
+
+
+#python compound intrest calculator:
+
+# principle = 0
+# rate = 0
+# time = 0
+
+# while principle <= 0:
+#     principle = float(input("Enter the principal amount: "))
+#     if principle <= 0:
+#         print("principle can't be aqual to or less than zero")
+ 
+# print(principle)
+
+# while rate <= 0:
+#     rate = float(input("Enter the intrest rate amount: "))
+#     if rate <= 0:
+#         print("intrest rate can't be aqual to or less than zero")
+
+# while time <= 0:
+#     time = int(input("Enter the time in years: "))
+#     if time <= 0:
+#         print("time can't be aqual to or less than zero")
+
+# total = principle *pow((1+rate/100), time)
+
+# print(f"balance after {time} year/s: \u20B9{total:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
